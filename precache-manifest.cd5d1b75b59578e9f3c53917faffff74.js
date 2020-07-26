ab721e970f307659a3077acb1eb986c135c1efe1
sriman577 <17pa1a0577@vishnu.edu.in>
2020-07-26 20:18:27 +0530

    Updates

---
+++ precache-manifest.cd5d1b75b59578e9f3c53917faffff74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e182ea1ddc19f98947aa24d04d1713e7",
+    "url": "/Simple_Blog_App/index.html"
+  },
+  {
+    "revision": "9dcfaac86456fa84a0a8",
+    "url": "/Simple_Blog_App/static/js/2.da25379c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple_Blog_App/static/js/2.da25379c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61b6621a21eb287eb91",
+    "url": "/Simple_Blog_App/static/js/main.edeb078f.chunk.js"
+  },
+  {
+    "revision": "8ac9b14a854f915ad21e",
+    "url": "/Simple_Blog_App/static/js/runtime-main.de6b4813.js"
+  }
+]);
